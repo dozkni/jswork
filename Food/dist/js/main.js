3b7d9dad693d2181cd6ecf2e2818992715521fa1
Dozkni <dozkni@gmail.com>
2020-11-16 18:29:15 +0600

    ddd

--- Food/dist/js/main.js
+++ Food/dist/js/main.js
@@ -4,6 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
     startTimer();
     modalWindow();
 
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        '.menu .container'
+    ).render();
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        16,
+        '.menu .container'
+    ).render();
+
 });
 
 function manageTabs() {
@@ -46,7 +71,7 @@ function manageTabs() {
 
 function startTimer() {
 
-    const deadline = '2020-11-15';
+    const deadline = '2020-11-20';
 
     function getTimeRemaining(enddate) {
         const t = Date.parse(enddate) - Date.parse(new Date()),
@@ -172,11 +197,33 @@ function modalWindow() {
 }
 
 class MenuCard {
-    constructor(src, alt, title, descr, price) {
+    constructor(src, alt, title, descr, price, parentSelector) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 68;
+        this.changeToRUR();
+    }
+
+    changeToRUR() {
+        this.price *= this.transfer;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.classList.add('menu__item');
+        element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>`;
+        this.parent.append(element);
     }
 }
--- _Food/dist/js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const tabs = document.querySelectorAll('.tabcontent');
-})
