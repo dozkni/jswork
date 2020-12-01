9ac2791da3b3af238eedd64c1faee4cd7afbbca8
Dozkni <dozkni@gmail.com>
2020-12-02 00:43:06 +0600

    slider

--- Food/js/main.js
+++ Food/js/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     startTimer();
     modalWindow();
     renderMenuCard();
+    manageSlides();
 
     // const panel = new Customizator();
     // panel.render();
@@ -311,3 +312,35 @@ function renderMenuCard() {
     
 }
 
+function manageSlides() {
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    total.textContent = String(slides.length).padStart(2, '0');
+
+    function showSlides(n) {
+        if (n > slides.length) slideIndex = 1;
+        if (n < 1) slideIndex = slides.length;
+        
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        current.textContent = String(slideIndex).padStart(2, '0');
+    }
+
+    function addSlideIndex(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => addSlideIndex(-1));
+    next.addEventListener('click', () => addSlideIndex(1));
+
+}
