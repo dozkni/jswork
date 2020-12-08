f61072e9e70af9c2e07f5c359d74276376baadba
max <kvint-car@mail.ru>
2020-12-09 01:44:48 +0600

    food

--- Food/js/main.js
+++ Food/js/main.js
@@ -315,32 +315,112 @@ function renderMenuCard() {
 function manageSlides() {
 
     const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width,
+          widthFrame = +width.slice(0, width.length - 2);
 
-    let slideIndex = 1;
-    showSlides(slideIndex);
+    let slideIndex = 1,
+        offset = 0;
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        if (i == 0) dot.style.opacity = 1;
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    slidesWrapper.style.width = width;
 
     total.textContent = String(slides.length).padStart(2, '0');
+    current.textContent = String(slideIndex).padStart(2, '0');
 
-    function showSlides(n) {
-        if (n > slides.length) slideIndex = 1;
-        if (n < 1) slideIndex = slides.length;
-        
-        slides.forEach(item => item.style.display = 'none');
+    next.addEventListener('click', () => {
+        if (offset == widthFrame * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += widthFrame;
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        slideIndex++;
+        changeSlideIndex();
+    });
 
-        slides[slideIndex - 1].style.display = 'block';
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = widthFrame * (slides.length - 1);
+        } else {
+            offset -= widthFrame;
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        slideIndex--;
+        changeSlideIndex();
+    });
 
+    function changeSlideIndex() {
+        if (slideIndex > slides.length) slideIndex = 1;
+        if (slideIndex < 1) slideIndex = slides.length;
         current.textContent = String(slideIndex).padStart(2, '0');
+        changeDots();
     }
 
-    function addSlideIndex(n) {
-        showSlides(slideIndex += n);
+    function changeDots() {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
     }
 
-    prev.addEventListener('click', () => addSlideIndex(-1));
-    next.addEventListener('click', () => addSlideIndex(1));
+    dots.forEach(dot => {
+        dot.addEventListener('click', e => {
+            slideIndex = e.target.getAttribute('data-slide-to');
+            offset = widthFrame * (slideIndex - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            changeSlideIndex(); 
+        });
+    });
+
+    // showSlides(slideIndex);
+
+    // total.textContent = String(slides.length).padStart(2, '0');
+
+    // function showSlides(n) {
+    //     if (n > slides.length) slideIndex = 1;
+    //     if (n < 1) slideIndex = slides.length;
+        
+    //     slides.forEach(item => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     current.textContent = String(slideIndex).padStart(2, '0');
+    // }
+
+    // function addSlideIndex(n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', () => addSlideIndex(-1));
+    // next.addEventListener('click', () => addSlideIndex(1));
 
 }
