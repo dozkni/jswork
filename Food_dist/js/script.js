5637654e53ebc413b0ba758adca178be2e8689c0
dozkni <dozkni@gmail.com>
2021-05-23 02:20:33 +0600

    add

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -94,4 +94,54 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadLine);
 
+    // MODAL
+
+    const modal = document.querySelector('.modal'),
+          modalTrigger = document.querySelectorAll('[data-modal]'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function showModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function hideModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', showModal);
+    });
+
+    modalCloseBtn.addEventListener('click', hideModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) hideModal();
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) hideModal();
+    });
+
+    setTimeout(showModal, 5000);
+
+    function showModalInTheEnd() {
+        if (window.pageYOffset + document.documentElement.clientHeight >=
+            document.documentElement.scrollHeight) {
+                showModal();
+                window.removeEventListener('scroll', showModalInTheEnd);
+            }
+    }
+
+    window.addEventListener('scroll', showModalInTheEnd);
+
+    const x1 = document.querySelector('#x1'),
+          x2 = document.querySelector('#x2');
+
+    window.addEventListener('scroll', () => {
+        x1.innerHTML = Math.floor(window.pageYOffset + document.documentElement.clientHeight);
+        x2.innerHTML = Math.floor(document.documentElement.scrollHeight);
+    });
+
 });
